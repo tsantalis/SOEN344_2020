6680b8de2bf13560edc525f8cabe0f90c06affdc
tsantalis <tsantalis@gmail.com>
2020-01-09 14:16:55 -0500

    Added implementation for computeCost() in Order

--- BookStore/src/bookstore/Order.java
+++ BookStore/src/bookstore/Order.java
@@ -5,4 +5,13 @@ import java.util.List;
 public class Order {
 
 	private List<Book> books;
+	private String id;
+	
+	public double computeCost() {
+		double totalCost = 0;
+		for(Book book : books) {
+			totalCost += book.computeCost();
+		}
+		return totalCost;
+	}
 }
