9d95961c0e0dc550a7562e9f354f00e02cc58976
tsantalis <tsantalis@gmail.com>
2020-01-09 14:20:58 -0500

    Implemented computeCost() in Customer

--- BookStore/src/bookstore/Customer.java
+++ BookStore/src/bookstore/Customer.java
@@ -1,5 +1,17 @@
 package bookstore;
 
+import java.util.List;
+
 public class Customer {
 
+	private List<Order> orders;
+	private String name;
+	
+	public double computeCost() {
+		double totalCost = 0;
+		for(Order order : orders) {
+			totalCost += order.computeCost();
+		}
+		return totalCost;
+	}
 }
