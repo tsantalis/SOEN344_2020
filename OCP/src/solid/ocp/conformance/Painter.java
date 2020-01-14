cf810b1508e67cd140356535f2c076d7a6105845
tsantalis <tsantalis@gmail.com>
2020-01-14 14:46:32 -0500

    Made Painter class depend only on Drawable interface

--- OCP/src/solid/ocp/conformance/Painter.java
+++ OCP/src/solid/ocp/conformance/Painter.java
@@ -3,9 +3,13 @@ package solid.ocp.conformance;
 import java.awt.Graphics;
 import java.util.List;
 
+
 public class Painter {
+	private List<Drawable> drawables;
 	
 	public void paintComponent(Graphics g) {
-		
+		for(Drawable drawable : drawables) {
+			drawable.draw(g);
+		}
 	}
 }
