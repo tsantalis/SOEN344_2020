843264a6848d9afbc8d257e802e7e2b217275fad
tsantalis <tsantalis@gmail.com>
2020-01-21 13:58:57 -0500

    Introduced constructor for TokenizerToIteratorAdapter

--- Adapter/src/adapter/TokenizerToIteratorAdapter.java
+++ Adapter/src/adapter/TokenizerToIteratorAdapter.java
@@ -1,7 +1,13 @@
 package adapter;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class TokenizerToIteratorAdapter implements Iterator<String> {
+
+	private StringTokenizer tokenizer;
+	public TokenizerToIteratorAdapter(StringTokenizer tokenizer) {
+		this.tokenizer = tokenizer;
+	}
 
 	@Override
 	public boolean hasNext() {
