d9af77fb5c62f5d0dd8c551b9fab9eca8ae65cda
tsantalis <tsantalis@gmail.com>
2020-01-23 14:45:05 -0500

    Initialized contents field in Directory with an empty ArrayList of AbstractFiles

--- Composite/src/composite/Directory.java
+++ Composite/src/composite/Directory.java
@@ -1,7 +1,9 @@
 package composite;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Directory extends AbstractFile {
-	private List<AbstractFile> contents;
+	private List<AbstractFile> contents = new ArrayList<AbstractFile>();
+
 }
