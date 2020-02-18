4433fb03181e68a27dc91ffc758cc91c5705a54e
tsantalis <tsantalis@gmail.com>
2020-02-18 15:02:16 -0500

    Implemented RobotController playing the role of Invoker in Command pattern

--- Command/src/command/RobotController.java
+++ Command/src/command/RobotController.java
@@ -1,23 +1,43 @@
 package command;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 //plays the role of the Invoker in the Command design pattern
 public class RobotController {
 
+	private LinkedList<RobotCommand> executionQueue = new LinkedList<RobotCommand>();
+	private Stack<RobotCommand> undoStack = new Stack<RobotCommand>();
+	
 	public RobotController() {
 		
 	}
 
 	public void addCommand(RobotCommand command) {
-		
+		executionQueue.add(command);
 	}
 
 	public void executeCommands() {
 		System.out.println("EXECUTING COMMANDS.");
-		
+		while(!executionQueue.isEmpty()) {
+			RobotCommand command = executionQueue.removeFirst();
+			command.execute();
+			undoStack.add(command);
+		}
 	}
 
 	public void undoCommands(int numUndos) {
 		System.out.format("REVERSING %d COMMAND(S).%n", numUndos);
-		
+		for(int i=0; i<numberOfIterations(numUndos); i++) {
+			undoStack.pop().undo();
+		}
+	}
+
+	private int numberOfIterations(int numUndos) {
+		return validInput(numUndos) ? numUndos : undoStack.size();
+	}
+
+	private boolean validInput(int numUndos) {
+		return numUndos > 0 && numUndos <= undoStack.size();
 	}
 }
